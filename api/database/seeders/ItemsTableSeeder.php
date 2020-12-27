79ef77a56399ae58bd20d998a18c8142db6955ea
Tony Zheng <tony@visionsoft.com.au>
2020-12-27 19:32:21 +1100

    added items migration and seeder

--- api/database/seeders/ItemsTableSeeder.php
+++ api/database/seeders/ItemsTableSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Item;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class ItemsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -13,6 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(ItemsTableSeeder::class);
+       Item::factory()->count(100)->create();
     }
 }
